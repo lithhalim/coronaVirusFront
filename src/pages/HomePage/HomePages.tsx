ccadd0852f4052fa38488e3a59aca5c16772c39b
lith osama zuhair abdel halim <103363189+lithhalim@users.noreply.github.com>
2023-03-02 15:18:35 +0300

    Update HomePages.tsx

--- src/pages/HomePage/HomePages.tsx
+++ src/pages/HomePage/HomePages.tsx
@@ -4,12 +4,35 @@ import GetDataSpecificCoutry from './combonants/DateAndCountryInformation/GetDat
 import TotalSection from './combonants/TotalSection/TotalSection';
 
 function HomePages() {
+    const downloadFile = async (_) => {
+        fetch('https://pricingtemplates.s3.amazonaws.com/List+of+members.xlsx', { 
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/vnd.ms-excel', 
+            }
+          })
+          .then(response => response.blob())
+          .then(blob => {
+            const url = window.URL.createObjectURL(new Blob([blob]));
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = 'List.xlsx';
+            document.body.appendChild(a);
+            a.click();
+            a.remove();
+          });
+      };
 
 
   return (
     <div style={{zIndex:1}}>
         <TotalSection/> 
-        <GetDataSpecificCoutry/>       
+        <GetDataSpecificCoutry/>   
+        <div  className={"add"} onClick={downloadFile}>
+        <span><BsCloudDownloadFill/></span>
+          "PricingPage.download List Member"
+        </div>
+
     </div>
   )
 }
